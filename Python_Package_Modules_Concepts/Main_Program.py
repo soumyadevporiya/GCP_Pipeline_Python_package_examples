55bb8bdc24e0842644ae4fb3958556dd59f64254
Soumyadev Desktop <soumyadev.poriya@gmail.com>
2022-10-19 19:10:21 +0530

    main program py version added

---
+++ Python_Package_Modules_Concepts/Main_Program.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+import Distribution.Normal_Distribution as nd
+
+
+# In[16]:
+
+
+import pandas as pd
+
+df = nd.Generate_Dist(2,0.5,1000)
+
+
+# In[17]:
+
+
+print(df)
+
+
+# In[18]:
+
+
+dir(nd)
+
+
+# In[ ]:
+
+
+
+
